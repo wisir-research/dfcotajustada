6bb9218332bdfff21a52a080eb5ee6516f3a4abc
GiordanoGava <gavagiordano@gmail.com>
2024-07-24 14:16:17 -0300

    Atualizado

--- source/dfcotajustadaauto.py
+++ source/dfcotajustadaauto.py
@@ -29,27 +29,27 @@ def workday() -> datetime:
 USER = Path(os.path.expanduser("~"))
 
 # %%
-excel_path = USER / r"C:\Users\GiordanoBrunoGava\OneDrive - WISIR\General - WISIR\3 - OPERACIONAL\1 - DADOS\01 - PYTHON\02 - CONSOLIDADOS\COTAJ_ACAO_CONSOLIDADO.xlsx"
+excel_path = USER / r"C:\Users\GiordanoBrunoGava\OneDrive - WISIR\General\3 - OPERACIONAL\1 - DADOS\01 - PYTHON\02 - CONSOLIDADOS\COTAJ_ACAO_CONSOLIDADO.xlsx"
 dados_cotacao__aj_consolidado_excel = pd.read_excel(excel_path)
 
 # %%
 dados_cotacao__aj_consolidado_excel
 
 # %%
-dados_cotacao__aj_consolidado_excel['DATA'] = pd.to_datetime(dados_cotacao__aj_consolidado_excel['DATA'], format="%d/%m/%Y")
+dados_cotacao__aj_consolidado_excel['Data'] = pd.to_datetime(dados_cotacao__aj_consolidado_excel['Data'], format="%d/%m/%Y")
 
 # %%
-ultima_linha_com_data = dados_cotacao__aj_consolidado_excel['DATA'].last_valid_index()
+ultima_linha_com_data = dados_cotacao__aj_consolidado_excel['Data'].last_valid_index()
 
 # %%
 # CONSTRUIR O PAYLOAD COM AS DATAS DINÂMICAS
-data_ini=(datetime.now() - timedelta(days=60)).strftime("%d/%m/%Y")
-data_fim=datetime.now().strftime("%d/%m/%Y") 
+data_ini_1=(datetime.now() - timedelta(days=180)).strftime("%d/%m/%Y")
+data_fim_1=(datetime.now() - timedelta(days=120)).strftime("%d/%m/%Y")
 
 url = "https://www.comdinheiro.com.br/Clientes/API/EndPoint001.php"
 payload = (
     f"username=wisir.research&password=wisir.research&URL=HistoricoIndicadoresFundamentalistas001.php%3F"
-    f"%26data_ini%3D{data_ini}%26data_fim%3D{data_fim}"
+    f"%26data_ini%3D{data_ini_1}%26data_fim%3D{data_fim_1}"
     "%26trailing%3D12%26conv%3DMIXED%26moeda%3DMOEDA_ORIGINAL%26c_c%3Dconsolidado%26m_m%3D1000000"
     "%26n_c%3D2%26f_v%3D0%26papel%3Dexplode%28STOCK_GUIDE_WISIR%29"
     "%26indic%3DPRECO_AJ%28%2C%2C%2CA%2CC%29%26periodicidade%3Ddu%26graf_tab%3Dtabela%26desloc_data_analise%3D1"
@@ -59,20 +59,84 @@ payload = (
         
 headers = {"Content-Type": "application/x-www-form-urlencoded"}
 querystring = {"code": "import_data"}
-        
+
 #SOLICITAÇÃO DA API
-response = requests.post(url, data=payload, headers=headers, params=querystring, timeout=1800)
+response_1 = requests.post(url, data=payload, headers=headers, params=querystring, timeout=1800)
 
 # %%
-novos_dados = response.json()
+# CONSTRUIR O PAYLOAD COM AS DATAS DINÂMICAS
+data_ini_2=(datetime.now() - timedelta(days=119)).strftime("%d/%m/%Y")
+data_fim_2=(datetime.now() - timedelta(days=60)).strftime("%d/%m/%Y")
+
+url = "https://www.comdinheiro.com.br/Clientes/API/EndPoint001.php"
+payload = (
+    f"username=wisir.research&password=wisir.research&URL=HistoricoIndicadoresFundamentalistas001.php%3F"
+    f"%26data_ini%3D{data_ini_2}%26data_fim%3D{data_fim_2}"
+    "%26trailing%3D12%26conv%3DMIXED%26moeda%3DMOEDA_ORIGINAL%26c_c%3Dconsolidado%26m_m%3D1000000"
+    "%26n_c%3D2%26f_v%3D0%26papel%3Dexplode%28STOCK_GUIDE_WISIR%29"
+    "%26indic%3DPRECO_AJ%28%2C%2C%2CA%2CC%29%26periodicidade%3Ddu%26graf_tab%3Dtabela%26desloc_data_analise%3D1"
+    "%26flag_transpor%3D0%26c_d%3Dc%26enviar_email%3D0%26enviar_email_log%3D0%26cabecalho_excel%3Dmodo1"
+    "%26relat_alias_automatico%3Dcmd_alias_01&format=json3"
+)
+        
+headers = {"Content-Type": "application/x-www-form-urlencoded"}
+querystring = {"code": "import_data"}
+
+#SOLICITAÇÃO DA API
+response_2 = requests.post(url, data=payload, headers=headers, params=querystring, timeout=1800)
 
 # %%
-data = list()
-for lin in (table := response.json()["tables"]["tab0"]):
-    data.append(table[lin])
+# CONSTRUIR O PAYLOAD COM AS DATAS DINÂMICAS
+data_ini_3=(datetime.now() - timedelta(days=59)).strftime("%d/%m/%Y")
+data_fim_3=datetime.now().strftime("%d/%m/%Y") 
+
+url = "https://www.comdinheiro.com.br/Clientes/API/EndPoint001.php"
+payload = (
+    f"username=wisir.research&password=wisir.research&URL=HistoricoIndicadoresFundamentalistas001.php%3F"
+    f"%26data_ini%3D{data_ini_3}%26data_fim%3D{data_fim_3}"
+    "%26trailing%3D12%26conv%3DMIXED%26moeda%3DMOEDA_ORIGINAL%26c_c%3Dconsolidado%26m_m%3D1000000"
+    "%26n_c%3D2%26f_v%3D0%26papel%3Dexplode%28STOCK_GUIDE_WISIR%29"
+    "%26indic%3DPRECO_AJ%28%2C%2C%2CA%2CC%29%26periodicidade%3Ddu%26graf_tab%3Dtabela%26desloc_data_analise%3D1"
+    "%26flag_transpor%3D0%26c_d%3Dc%26enviar_email%3D0%26enviar_email_log%3D0%26cabecalho_excel%3Dmodo1"
+    "%26relat_alias_automatico%3Dcmd_alias_01&format=json3"
+)
+        
+headers = {"Content-Type": "application/x-www-form-urlencoded"}
+querystring = {"code": "import_data"}
+
+#SOLICITAÇÃO DA API
+response_3 = requests.post(url, data=payload, headers=headers, params=querystring, timeout=1800)
 
 # %%
-dados_cotacao__aj_novos = pd.DataFrame(data)
+data_1 = list()
+for lin in (table := response_1.json()["tables"]["tab0"]):
+    data_1.append(table[lin])
+
+# %%
+dados_cotacao__aj_novos_1 = pd.DataFrame(data_1)
+
+# %%
+data_2 = list()
+for lin in (table := response_2.json()["tables"]["tab0"]):
+    data_2.append(table[lin])
+
+# %%
+dados_cotacao__aj_novos_2 = pd.DataFrame(data_2)
+
+dados_cotacao__aj_novos_2 = dados_cotacao__aj_novos_2.iloc[1:].reset_index(drop=True)
+
+# %%
+data_3 = list()
+for lin in (table := response_3.json()["tables"]["tab0"]):
+    data_3.append(table[lin])
+
+# %%
+dados_cotacao__aj_novos_3 = pd.DataFrame(data_3)
+
+dados_cotacao__aj_novos_3 = dados_cotacao__aj_novos_3.iloc[1:].reset_index(drop=True)
+
+# %%
+dados_cotacao__aj_novos = pd.concat([dados_cotacao__aj_novos_1, dados_cotacao__aj_novos_2, dados_cotacao__aj_novos_3], ignore_index=True) 
 
 # %%
 dados_cotacao__aj_novos = dados_cotacao__aj_novos.rename(
@@ -101,13 +165,13 @@ dados_cotacao__aj_novos = dados_cotacao__aj_novos.drop(index = 0)
 parser.parse("12-06-2024", dayfirst = True)
 
 # %%
-dados_cotacao__aj_consolidado_excel
+dados_cotacao__aj_novos
 
 # %%
-dados_cotacao__aj_novos["Data"] = dados_cotacao__aj_novos["Data"].map(lambda row: parser.parse(row, dayfirst = True))
+#dados_cotacao__aj_novos["Data"] = dados_cotacao__aj_novos["Data"].map(lambda row: parser.parse(row, dayfirst = True))
 
 # %%
-dados_cotacao__aj_novos = dados_cotacao__aj_novos.rename(columns = {"Data": "DATA"}).astype(dtype = {"DATA": "datetime64[ns]"})
+#dados_cotacao__aj_novos = dados_cotacao__aj_novos.rename(columns = {"Data": "DATA"}).astype(dtype = {"DATA": "datetime64[ns]"})
 
 # %%
 dados_cotacao__aj_novos.reset_index(drop = True)
@@ -127,7 +191,7 @@ dados_cotacao__aj_novos = dados_cotacao__aj_novos.loc[:, ~dados_cotacao__aj_novo
 
 # %%
 # SALVA O DF CONCATENANDO NO EXCEL
-output_path = USER / r'OneDrive - WISIR\General - WISIR\3 - OPERACIONAL\1 - DADOS\01 - PYTHON\02 - CONSOLIDADOS\COTAJ_ACAO_CONSOLIDADO.xlsx'
+output_path = USER / r'OneDrive - WISIR\General\3 - OPERACIONAL\1 - DADOS\01 - PYTHON\02 - CONSOLIDADOS\COTAJ_ACAO_CONSOLIDADO.xlsx'
 
 dados_cotacao__aj_novos = dados_cotacao__aj_novos.drop(dados_cotacao__aj_novos.index[-1])
 dados_cotacao__aj_novos.to_excel(output_path, index=False)
